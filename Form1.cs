b1680c7e130458122861d507e0924f0f0e647b60
AbdullahIbrahim23 <AbdullahIbrahim23@users.noreply.github.com>
2017-10-03 13:22:19 -0400

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangingHealthWithTimer
+{
+    public static class Util
+    {
+        private static Random rnd = new Random();
+        public static int GetRandom()
+        {
+            return rnd.Next(1, 6);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Tick += new EventHandler(timer1_Tick);
+
+            timer.Interval = (1000) * (3);
+            timer.Enabled = true;
+            timer.Start();         
+        }
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            int a = int.Parse(label2.Text);
+            var i = Util.GetRandom();
+            
+
+            a -= i;
+
+            label2.Text = a.ToString();
+
+            Form2 Form = new Form2();
+            Form.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangingHealthWithTimer
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            label1.Text = "Enemy attacks you! -"+j+"  HP";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
