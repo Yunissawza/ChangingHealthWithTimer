a3dfc413ab17e75a5c867ed1165a53f3b1b02f04
20hz20khz <20hz20khz@gmail.com>
2017-10-04 00:52:43 -0400

    cleaned up

--- Form1.cs
+++ Form1.cs
@@ -8,41 +8,70 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/* This code is based on the documentation for the System.Windows.Forms.Timer posted here: https://msdn.microsoft.com/en-us/library/system.windows.forms.timer.tick(v=vs.110).aspx */
+
 namespace ChangingHealthWithTimer
 {
-    public static class Util
+    // Create a Dice class
+    public static class Dice
     {
         private static Random rnd = new Random();
-        public static int GetRandom()
+
+        public static int Roll()
         {
-            return rnd.Next(1, 6);
+          // Returns an integer 1 to 6
+          return rnd.Next(1, 6);
         }
     }
     public partial class Form1 : Form
     {
-        Timer timer = new Timer();
-        
+        // Declare a static Timer object called myTimer using the new Timer() constructor
+
+        // Declare a static boolean called exitFlag and set it to false
+
+        // Declare a static int called
+
         public Form1()
         {
-            InitializeComponent();
+          InitializeComponent();
 
-            timer.Tick += new EventHandler(timer1_Tick);
+          // Run the timer1_Tick function each time the Interval is up
+          mytimer.Tick += new EventHandler(timer1_Tick);
 
-            timer.Interval = (1000) * (3);
-            timer.Enabled = true;
-            timer.Start();         
+          // Set your timer's Interval property to a random 1-6 seconds using the Dice class's Roll method
+
+          // Set your timer's Enable property to true
+
+          // Start your timer using the Timer class's Start method
+
+          // Use a while loop to repeat while the exitFlag is false
+
+          {
+            // This code processes all the events in the queue.
+            Application.DoEvents();
+          }
+
+          // Stop your timer using the Timer class's Stop method
+
+
+        return 0;
         }
+
         public void timer1_Tick(object sender, EventArgs e)
         {
-            int a = int.Parse(label2.Text);
-            int i = Util.GetRandom();
+          // use int.Parse(label2.Text) to get the current health and store it in a new int called playerHealth
 
-            a -= i;
+          // ROLL TWO DICE: use the Dice class's Roll method + another Dice Roll method and store it in a new int called enemyDamage
 
-            label2.Text = a.ToString();
+          // Set playerHealth to playerHealth minus enemyDamage
+          playerHealth -= enemyDamage;
 
-            Form2 Form = new Form2();
-            Form.Show();
+          // create a MessageBox with a Show method that tells you "Enemy attacks you! -enemyDamage HP"
+
+          // convert playerHealth with the ToString() method and set it as the label2.Text
+
+          // if the playerHealth is less than 1, set the exitFlag to true
+
         }
     }
 }
